5aab6c885b15a4d4b68020224cebd6b2c0be3079
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-02-01 17:05:52 +0100

    task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
 Write a Python script that, using this REST API
-, for a given employee ID, returns information about his/her 
-TODO list progress. 
+, for a given employee ID, returns information about his/her
+TODO list progress
 """
 
 from requests import get
