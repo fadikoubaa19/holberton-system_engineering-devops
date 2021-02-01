e4d0197d978cbde0029587e08352d20e719cdc10
fadikoubaa19 <fadi.koubaa@gmail.com>
2021-02-01 19:53:14 +0100

    task1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,20 +11,20 @@ import csv
 if __name__ == "__main__":
 
     url_name = "https://jsonplaceholder.typicode.com/"
-    TID = argv[1]
+    ID = argv[1]
 
     USERNAME = get(url_name + "users",
-                   params={"id": TID}).json()[0].get('username')
+                   params={"id": ID}).json()[0].get('username')
 
     TOTAL_NUMBER_OF_TASKS = get(url_name + "todos",
-                                params={"userId": TID}).json()
+                                params={"userId": ID}).json()
 
     NUMBER_OF_DONE_TASKS = get(url_name + "todos",
-                               params={"userId": TID,
+                               params={"userId": ID,
                                        "completed": "true"}).json()
 
     with open('{}.csv'.format(ID), 'w') as csvfile:
         for item in TOTAL_NUMBER_OF_TASKS:
             l = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-            l.writerow([TID, USERNAME, item.get('completed'),
+            l.writerow([ID, USERNAME, item.get('completed'),
                         item.get('title')])
