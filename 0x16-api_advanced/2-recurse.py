983e0c3bdf515c5aaab6f671d60a7d37eedfe1f5
fadikoubaa19 <2062@holbertonschool.com>
2021-02-04 23:14:18 +0100

    ll

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -12,8 +12,7 @@ function number_of_subscirbers
 def recurse(subreddit, hot_list=[]):
     try:
         url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-        req = requests.get(url, headers={'User-Agent': 'fedi'},
-                         params={'limit': 100})
+        req = requests.get(url, headers={'User-Agent': 'f'}), para = {'l': 100}
         data = req.json()
         hot_list.extend(data['data']['children'])
         page = data['data']['after']
@@ -30,8 +29,8 @@ def recursive(subreddit, hot_list=[], page=""):
     if page is None:
         return
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    req = requests.get(url, headers={'User-Agent': 'fedi'},
-                     params={'after': page, 'limit': 100})
+    req = requests.get(url, headers={'User-Agent': 'f'}),
+    para = {'after': page, 'l': 100}
     data = req.json()
     hot_list.extend(data['data']['children'])
     return recursive(subreddit, hot_list, data['data']['after'])
