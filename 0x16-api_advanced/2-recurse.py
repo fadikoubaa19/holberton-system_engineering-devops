9daf6aa8c43a1c93f1b9fb3c4f63d0835f99dcac
fadikoubaa19 <2062@holbertonschool.com>
2021-02-04 22:59:55 +0100

    only rec

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+subs counting
+"""
+import requests
+from sys import argv
+"""
+function number_of_subscirbers
+"""
+
+
+def recurse(subreddit, hot_list=[]):
+    try:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+        req = requests.get(url, headers={'User-Agent': 'fedi'},
+                         params={'limit': 100})
+        data = req.json()
+        hot_list.extend(data['data']['children'])
+        page = data['data']['after']
+        if data['data']['after'] is None:
+            return
+    except KeyError:
+        return None
+    recursive(subreddit, hot_list, page)
+    return hot_list
+
+
+def recursive(subreddit, hot_list=[], page=""):
+    """handles the other entries"""
+    if page is None:
+        return
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    req = requests.get(url, headers={'User-Agent': 'fedi'},
+                     params={'after': page, 'limit': 100})
+    data = req.json()
+    hot_list.extend(data['data']['children'])
+    return recursive(subreddit, hot_list, data['data']['after'])
