95457d0b4b16cb68ab0f95854d27a5c47b05c1b0
fadikoubaa19 <2062@holbertonschool.com>
2021-02-04 22:26:02 +0100

    top ten task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+subs counting
+"""
+import requests
+from sys import argv
+"""
+function number_of_subscirbers
+"""
+
+def top_ten(subreddit):
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    try:
+        req = requests.get(url, headers={'User-agent': 'fedi'})
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
+
+
+if __name__ == "__main__":
+    top_ten(argv[1])
